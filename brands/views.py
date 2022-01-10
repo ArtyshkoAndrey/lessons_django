e75295dee91732836a2743ea7e7940e176ce31e4
Fulliton <artyshko.andrey@gmail.com>
2022-01-10 20:31:24 +0700

    Views index brands

--- brands/views.py
+++ brands/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.http import HttpResponse
+from django.core import serializers
+from .models import Brand
 
-# Create your views here.
+
+def index(request):
+    # Проверяем что бы был GET запрос, иначе 404 ошибка
+    if request.method == 'GET':
+        # Создам запрос на выборку всех данных из БД
+        brands = Brand.objects.all()
+
+        # Если имеется параметр 'name' в запросе
+        if 'name' in request.GET:
+            # Создаём запрос на фильтрацию всех брендов где имеется в имени часть name
+            # SELECT * FROM brands WHERE 'name' LIKE '*****'
+            brands = brands.filter(name__contains=request.GET.get('name'))
+
+        # Представляем объекты в JSON формат
+        data = serializers.serialize("json", brands, fields=('name', 'description'))
+
+        # Возвращаем JSON ответ пользователю
+        return HttpResponse(data, content_type="application/json")
+    else:
+        return Http404()
